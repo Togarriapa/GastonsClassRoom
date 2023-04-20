5c55a98fcbd44b52b70ed1e325534d5e483b1ca3
Joao Carneiro <jmaiacac@gmail.com>
2023-04-20 12:47:36 +0100

    might be the final version

--- public/ClassRoomButtons.js
+++ public/ClassRoomButtons.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+
+  let player;
+
     document.querySelector(".gaston-icon").addEventListener("click", () => {
-  
   
       console.log("Event triggered")
       const speechBubble = document.querySelector("#SpeechBubble");
@@ -37,6 +39,25 @@ document.addEventListener("DOMContentLoaded", () => {
           console.log("trying to move left")
         // move left
         chatBox.removeAttribute('hidden');
+
+        const myBaloon = document.getElementById('SpeechBubble')
+        myBaloon.textContent = "No one does better than me!";
+
+        if (myBaloon.style.display === "none") {
+          myBaloon.style.position = "absolute";
+          myBaloon.style.display = "block";
+          myBaloon.style.animationName = "expand-bounce";
+          myBaloon.style.animationDuration = "3.25s";
+        } else {
+          myBaloon.style.animationName = "shrink";
+          myBaloon.style.animationDuration = "0.1s";
+    
+          setTimeout(() => {
+            myBaloon.style.display = "none";
+          }, 100);
+        }
+
+        
         
         const gastonImage = document.querySelector('.gaston-image');
         gastonImage.classList.add('gaston-image-move-left-2nd');
@@ -83,5 +104,121 @@ document.addEventListener("DOMContentLoaded", () => {
         speechSynthesis.speak(utterance);
       });
 
+      document.getElementById('cornoId').addEventListener('click', () => {
+
+
+        const myvideoWrapper = document.getElementById('video-wrapper');
+        if (myvideoWrapper.style.display === 'none') {
+
+          const myBaloon = document.getElementById('SpeechBubble')
+          myBaloon.textContent = "No one does better than me!";
+
+        if (myBaloon.style.display === "none") {
+          myBaloon.style.position = "absolute";
+          myBaloon.style.display = "block";
+          myBaloon.style.animationName = "expand-bounce";
+          myBaloon.style.animationDuration = "3.25s";
+        } else {
+          myBaloon.style.animationName = "shrink";
+          myBaloon.style.animationDuration = "0.1s";
     
+          setTimeout(() => {
+            myBaloon.style.display = "none";
+          }, 100);
+        }
+
+        const quadroElement = document.getElementById('quadroId');
+        if (quadroElement) {
+            quadroElement.style.display = 'none';
+        }
+        
+        const gastonImage = document.querySelector('.gaston-image');
+        gastonImage.classList.add('gaston-image-move-left-2nd');
+        gastonImage.classList.remove('gaston-image-move-right-2nd')
+        
+      } else {
+
+        // move right 
+
+        const quadroElement = document.getElementById('quadroId');
+        if (quadroElement) {
+            quadroElement.style.display = 'block';
+        }
+
+        pauseVideo();
+        console.log("trying to move right")
+        chatBox.setAttribute('hidden', '');
+        const gastonImage = document.querySelector('.gaston-image');
+        gastonImage.classList.add('gaston-image-move-right-2nd');
+        gastonImage.classList.remove('gaston-image-move-left-2nd')
+      }
+
+      const videoWrapper = document.getElementById('video-wrapper');
+      const toggleVideo = document.getElementById('toggle-video');
+  
+          if (videoWrapper.style.display === 'none') {
+              videoWrapper.style.display = 'block';
+              loadYouTubeVideo(); // Function to load the YouTube video
+          } else {
+              videoWrapper.style.display = 'none';
+          }
+        })
+
+
+
+function loadYouTubeVideo() {
+    const videoContainer = document.getElementById('video-container');
+    
+    // Check if the video is already loaded
+    if (videoContainer.innerHTML !== '') {
+        return;
+    }
+
+    // Replace the video ID with your desired video ID
+    const videoId = 'NzK5MutYGJg';
+
+    // Update the player variable to store the player instance
+    player = new YT.Player(videoContainer, {
+        height: '390',
+        width: '640',
+        videoId: videoId,
+        playerVars: {
+            'autoplay': 1, // Enable autoplay
+            'controls': 1 // Show player controls
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+  
+  function onPlayerReady(event) {
+      // You can use this function to perform any action when the player is ready
+  }
+  
+  function onPlayerStateChange(event) {
+      // You can use this function to perform any action when the player state changes
+  }
+    
+
+  function muteVideo() {
+    if (player && player.isMuted && !player.isMuted()) {
+        player.mute();
+    }
+}
+
+function unMuteVideo() {
+    if (player && player.isMuted && player.isMuted()) {
+        player.unMute();
+    }
+}
+
+function pauseVideo() {
+    if (player && player.pauseVideo) {
+        player.pauseVideo();
+    }
+}
+
+
   });
